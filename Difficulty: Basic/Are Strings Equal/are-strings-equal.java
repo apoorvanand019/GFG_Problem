e9d1007bb184aa9d7c4ab64c17cce3b28556cf9d
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2025-12-22 10:49:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Are Strings Equal/are-strings-equal.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    public static boolean areEqual(String s1, String s2) {
+        // Your code here
+        if(s1.equals(s2)){
+            return true;
+        }
+        return false;
+    }
+}
