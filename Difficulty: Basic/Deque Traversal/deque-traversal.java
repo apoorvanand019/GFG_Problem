db3759efd2532be598b145ba8cde4eb2128169eb
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2025-12-24 14:14:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Deque Traversal/deque-traversal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for(int x: deq){
+            System.out.print(x +" ");
+        }
+        System.out.println();
+    }
+}
