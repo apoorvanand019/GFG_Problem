c3257a673a1a0c88cd18558b5548088893c0d505
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2025-12-24 14:13:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square/print-square.java
+++ Difficulty: Basic/Print Square/print-square.java
@@ -1,17 +1,20 @@
-// User function Template for Java
-class Solution {
-    public static void square(int s) {
-        // Complete the code given below
-        for(int i=0;i<s;i++){
-            for(int j=0;j<s;j++){
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 
-                if(i == 0 || i == s-1 || j==0 || j==s-1){
+                if(i==0 || i==n-1 || j==0 || j==n-1){
                     System.out.print("* ");
                 }
                 else{
                     System.out.print("  ");
                 }
-                
             }
             System.out.println();
         }
