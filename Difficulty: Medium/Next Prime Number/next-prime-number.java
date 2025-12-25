e27de953679caca5737cedcea3a5c15651211cc2
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2025-12-25 11:16:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Prime Number/next-prime-number.java
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -1,21 +1,26 @@
 // User function Template for Java
 class Solution {
     public static int nextPrime(int n) {
-
+        
         // code here to find next prime number
-        int candidate=n + 1;
-        while(!isPrime(candidate)){
-            candidate++;
+        int num=n+1;
+        while(true){
+            
+        
+        if(isPrime(num)){
+            return num;
+        }
+        num++;
         }
         // return next prime number
-        return candidate;
-        
+     
     }
+    
     public static boolean isPrime(int n){
         if(n<=1){
             return false;
         }
-        for(int i=2;i<=(int)Math.sqrt(n);i++){
+        for(int i=2;i<=n/2;i++){
             if(n % i == 0){
                 return false;
             }
