d794a10fb24189aad6e2d540b47e5797377e4599
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2025-12-25 11:17:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        // Your Code Here
+        int x=sc.nextInt();
+        for(int i=x;i>=0;i--){
+            System.out.print(i + " ");
+        }
+    }
+}
