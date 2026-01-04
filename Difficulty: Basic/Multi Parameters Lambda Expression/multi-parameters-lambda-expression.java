dc708ea942ae6bc846bdc55e786c8b0e497996bc
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2026-01-04 20:47:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multi Parameters Lambda Expression/multi-parameters-lambda-expression.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+/*
+The interface looks like
+
+interface Add {
+    public int addParameters(int a, int b);
+}
+*/
+
+class Solution {
+    public static Add helperFunction() {
+        // Your code here
+        return (a,b) -> a+b;
+
+        // Implement the addParameters method that returns a+b.
+        
+        }
+    }
