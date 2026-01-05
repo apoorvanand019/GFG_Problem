c32adf6854f057cf4240b7d214d4bbd6d3aadbf5
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2026-01-05 10:33:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Equals Method/equals-method.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public static boolean areEqual(int a[], int b[]) {
+        // Your code here
+        if(a.length != b.length){
+            return false;
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
