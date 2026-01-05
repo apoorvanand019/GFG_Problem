1611915c44b04b0cabf79f7abc54d40a39d5b812
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2026-01-05 10:10:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fill Method/fill-method.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static void fillArray(int arr[], int k) {
+        // Your code here
+        for(int i=0;i<arr.length;i++){
+            arr[i]=k;
+        }
+
+        // You don't need to print
+    }
+}
