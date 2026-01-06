b77d0a80413aee3b3871e0dca76c1ad11dda98a1
apoorvanand019 <167872639+apoorvanand019@users.noreply.github.com>
2026-01-06 11:55:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/One Parameter Lambda Expression/one-parameter-lambda-expression.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+/*
+The interface looks like
+
+interface Multiply {
+    public int multiplyBy5(int n);
+}
+*/
+
+class Solution {
+
+    public static Multiply helperFunction() {
+        // Your code here
+        return  n -> n*5;
+
+        // Implement the multiplyBy5(int n) method using lambda expression. The
+        // implemented function should return n*5
+    }
+}
